1e81ef1eff6678b53e24ac15252cc45035cafeb6
RachelTWQ <racheltang15@hotmail.com>
2019-05-16 17:03:59 +0000

    add user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def created
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = user.id
+            redirect_to '/'
+        else
+            flash[:alert] = user.errors.full_messages
+            redirect_to '/registration'
+    end
+
+    protected user_params
+        params.require(:user).permit(
+            :first_name,
+            :last_name,
+            :email,
+            :password,
+            :password_confirmation
+        )
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+    has_secure_password
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :password, presence: true
+    validates :password, length: { minimum: 3 }
+    validates :password_confirmation, presence: true
+    # validates :email, :format => /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
 end
