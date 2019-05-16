6f5b878abc239322ab495067d14cdf747ab9515f
RachelTWQ <racheltang15@hotmail.com>
2019-05-16 18:13:00 +0000

    fix login logout and registration bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
 
     def new
-        @user = User.new
     end
 
-    def created
-        @user = User.new(user_params)
+    def create
+        user = User.new(user_params)
 
-        if @user.save
+        if user.save
             session[:user_id] = user.id
             redirect_to '/'
         else
             flash[:alert] = user.errors.full_messages
             redirect_to '/registration'
+        end
     end
 
-    protected user_params
+    private 
+    def user_params
         params.require(:user).permit(
             :first_name,
             :last_name,
