dc5ba85f8714cb35c5cb0cfede4eef9112d011d5
RachelTWQ <racheltang15@hotmail.com>
2019-05-21 19:45:57 +0000

    add feature test case for product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see individual product" do
+
+    visit root_path
+    first('.actions').click_link('Details')
+    
+    expect(page).to have_content "Description"
+    puts page.html
+    save_screenshot
+  end
+
 end
